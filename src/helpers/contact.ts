9dfab787bec1bdad267d769fcf2fbda5ef9de22b
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-06-08 13:33:02 +0500

    Saving new contact list

--- src/helpers/contact.ts
+++ src/helpers/contact.ts
@@ -1,4 +1,5 @@
 import { getChatList } from "../api/contact";
+import { handleContacts } from "../idb/contacts";
 import { store } from "../redux";
 import { setContactList } from "../redux/action/user";
 
@@ -7,9 +8,10 @@ export const handleContactList = async (
   receiverUserName: string,
   loggedInUserEmail: string
 ) => {
-  const { user: chatList } = await store.getState();
+  const { user: chatList, loggedInUserId } = await store.getState();
   if (!chatList[senderName] || !chatList[receiverUserName]) {
     const contactList = await getChatList(loggedInUserEmail);
     store.dispatch(setContactList(contactList));
+    await handleContacts(chatList, loggedInUserId);
   }
 };
