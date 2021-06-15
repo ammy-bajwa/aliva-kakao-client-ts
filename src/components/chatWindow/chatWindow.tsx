dae14bcbfb9fbeacb75d9184bec74eda742bce81
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-06-16 01:48:52 +0500

    Open image onclick

--- src/components/chatWindow/chatWindow.tsx
+++ src/components/chatWindow/chatWindow.tsx
@@ -34,42 +34,48 @@ const ChatWindow = (props: any) => {
     <div className="m-2" id="chatWindowContainer">
       {console.log("props: ", props)}
       <h1>Chat Window</h1>
-      {chat.length > 0 ? chat.map((message: any, index: number) => (
-        <div
-          key={index}
-          className={
-            message.received
-              ? "d-flex border-bottom m-2 text-light"
-              : "d-flex flex-row-reverse text-light"
-          }
-        >
-          {message.text === "photo" &&
-            message.attachment &&
-            message.attachment.thumbnailUrl && (
-              <img
-                loading="lazy"
-                alt="userImages"
-                src={message.attachment.thumbnailUrl}
-                width="90"
-                height="90"
-              />
-            )}
-          <span
-            className={`m-2 p-2 d-block w-100 rounded ${
-              currentFocus === message.receiverUserName
-                ? "receiverMessage border border-info"
-                : "senderMessage border border-dark"
-            }`}
-          >
-            {message.text}
-            <hr />
-            <span className="w-100 d-flex justify-content-between">
-              <span>{moment(message.sendAt).format("hh:mm:ss A")}</span>
-              <span>{moment(message.sendAt).format("DD/MM/YYYY")}</span>
-            </span>
-          </span>
-        </div>
-      )): "Message Will Be Here"}
+      {chat.length > 0
+        ? chat.map((message: any, index: number) => (
+            <div
+              key={index}
+              className={
+                message.received
+                  ? "d-flex border-bottom m-2 text-light"
+                  : "d-flex flex-row-reverse text-light"
+              }
+            >
+              {message.text === "photo" &&
+                message.attachment &&
+                message.attachment.thumbnailUrl && (
+                  <img
+                    loading="lazy"
+                    alt="userImages"
+                    src={message.attachment.thumbnailUrl}
+                    onClick={() =>
+                      window.open(message.attachment.url, "_blank")
+                    }
+                    className="hoverPointer"
+                    width="90"
+                    height="90"
+                  />
+                )}
+              <span
+                className={`m-2 p-2 d-block w-100 rounded ${
+                  currentFocus === message.receiverUserName
+                    ? "receiverMessage border border-info"
+                    : "senderMessage border border-dark"
+                }`}
+              >
+                {message.text}
+                <hr />
+                <span className="w-100 d-flex justify-content-between">
+                  <span>{moment(message.sendAt).format("hh:mm:ss A")}</span>
+                  <span>{moment(message.sendAt).format("DD/MM/YYYY")}</span>
+                </span>
+              </span>
+            </div>
+          ))
+        : "Message Will Be Here"}
     </div>
   );
 };
